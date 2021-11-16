70d6573b648e10675102c5401ac33b556f545512
eman2XR <emanuel.t.tomozei@gmail.com>
2021-11-16 12:50:16 +0000

    whole project

---
+++ Stargazer_UnityProject/Assets/Scripts/TestVoice.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TestVoice : MonoBehaviour
+{
+    public UnityEvent buttonPressed;
+
+    void Update()
+    {
+        if(OVRInput.GetDown(OVRInput.Button.Any) || Input.GetKeyDown("l"))
+        {
+            buttonPressed.Invoke();
+        }
+    }
+}

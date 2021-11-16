0ee8f9548cb74c8b80b5b4535d5aed820dbf2060
eman2XR <emanuel.t.tomozei@gmail.com>
2021-11-16 16:56:11 +0000

    added test vfx

---
+++ Stargazer_UnityProject/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    Vector3 targetPosition;
+
+    void Update()
+    {
+        if (OVRInput.Get(OVRInput.Button.PrimaryThumbstickUp) || OVRInput.Get(OVRInput.Button.SecondaryThumbstickUp))
+        {
+            targetPosition = Camera.main.transform.forward / 100;
+            this.transform.position += new Vector3(targetPosition.x, 0, targetPosition.z);
+        }
+        else if(OVRInput.Get(OVRInput.Button.PrimaryThumbstickDown) || OVRInput.Get(OVRInput.Button.SecondaryThumbstickDown))
+        {
+            targetPosition = -(Camera.main.transform.forward / 100);
+            this.transform.position += new Vector3(targetPosition.x, 0, targetPosition.z);
+        }
+        else if(OVRInput.Get(OVRInput.Button.PrimaryThumbstickRight) || OVRInput.Get(OVRInput.Button.SecondaryThumbstickRight))
+        {
+            targetPosition = (Camera.main.transform.right / 100);
+            this.transform.position += new Vector3(targetPosition.x, 0, targetPosition.z);
+        }
+        else if (OVRInput.Get(OVRInput.Button.PrimaryThumbstickLeft) || OVRInput.Get(OVRInput.Button.SecondaryThumbstickLeft))
+        {
+            targetPosition = -(Camera.main.transform.right / 100);
+            this.transform.position += new Vector3(targetPosition.x, 0, targetPosition.z);
+        }
+
+    }
+}

857bc23393ad871de23cf0747f8a05316de2ab96
eman2XR <emanuel.t.tomozei@gmail.com>
2021-11-20 23:42:02 +0000

    filming sess

--- Stargazer_UnityProject/Assets/StarMapping/Scripts/ConstellationsManager.cs
+++ Stargazer_UnityProject/Assets/StarMapping/Scripts/ConstellationsManager.cs
@@ -7,23 +7,34 @@ public class ConstellationsManager : MonoBehaviour
 {
     public GameObject nameTagPrefab;
 
-    private void Start()
+    IEnumerator Start()
     {
-        CreateNameTags();
+        //wait for starts to move
+        yield return new WaitForSeconds(2);
+        foreach (Transform trans in GetComponentInChildren<Transform>())
+        {
+            if (trans.GetComponent<Renderer>())
+            {
+                trans.GetChild(0).eulerAngles = new Vector3(trans.GetChild(0).eulerAngles.x, trans.GetChild(0).eulerAngles.y, 0);
+            }
+        }
     }
 
+
+    [ContextMenu("create name tags")]
     void CreateNameTags()
     {
         foreach (Transform trans in GetComponentInChildren<Transform>())
         {
             if (trans.GetComponent<Renderer>())
             {
-                GameObject nameTag = GameObject.Instantiate(nameTagPrefab, trans);
-                Vector3 direction = trans.transform.position - Camera.main.transform.position;
-                nameTag.transform.GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>().text = trans.name;
-                nameTag.transform.rotation = Quaternion.LookRotation(direction);
-                nameTag.transform.position = trans.position;
-                nameTag.transform.parent = trans;
+                //DestroyImmediate(trans.GetChild(0).gameObject);
+                //GameObject nameTag = GameObject.Instantiate(nameTagPrefab, trans);
+                //Vector3 direction = trans.transform.position - Camera.main.transform.position;
+                //nameTag.transform.GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>().text = trans.name;
+                //nameTag.transform.rotation = Quaternion.LookRotation(direction);
+                //nameTag.transform.position = trans.position;
+                //nameTag.transform.parent = trans;
             }
         }
     }
